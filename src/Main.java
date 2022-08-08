a5f000802b418a0777d6ff1c442038a49a38c4db
Ariel Fucilieri <soy.fucii@gmail.com>
2022-08-08 19:30:47 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        Person person= new Person();
+
+        person.setAge(20);
+        person.setName("Ariel");
+        person.setPhoneNumber(4499990);
+        System.out.println("Name: "+ person.getName());
+        System.out.println("Age: "+ person.getAge());
+        System.out.println("Phone Number: "+ person.getPhoneNumber());
+
+
+
+
+    }
+
+    static class Person{
+        private int age;
+        private String name;
+        private int phoneNumber;
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setPhoneNumber(int phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getPhoneNumber() {
+            return phoneNumber;
+        }
+    }
+}
